359e5ca59a9e9e4ada71a3d21e43c486bb3ba368
Sabina <sabina2020akm@gmail.com>
2025-12-12 23:53:36 +0500

    Initial commit

---
+++ src/com/sabina/vetclinic/main.java
@@ -0,0 +1,17 @@
+package com.sabina.vetclinic;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("Welcome to Vet Clinic Management System");
+        System.out.println();
+        System.out.println("The System helps manage veterinary clinic operations");
+        System.out.println();
+        System.out.println("Main features planned for development:");
+        System.out.println("- Register pets and their owner;");
+        System.out.println("- Store and update pet medical history;");
+        System.out.println("- Manage veterinarians and their schedules;");
+        System.out.println("- Record vaccinations and diagnoses;");
+        System.out.println();
+        System.out.println("Created by Sabina | Week 1 | OOP");
+    }
+}
